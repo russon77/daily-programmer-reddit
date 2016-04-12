6a8d08d4998592af67c38677094802f09fe4fb30
Tristan K <russon77@gmail.com>
2016-04-12 17:19:10 -0400

    it works! main and bonus completed for 8x8 grids. 12x12 theoretically work, but the algorithm is... memory intensive...

--- 261m/main.py
+++ 261m/main.py
@@ -43,26 +43,43 @@ def apply_permutation(original_list, new_list, p):
     return new_list
 
 
-def get_permutation(len):
-    starting_point = [x for x in range(0, len)]
-    yield starting_point
+def make_tree(datum, remaining):
+    if len(remaining) == 0:
+        return {
+            'p': datum,
+            'leaves': []
+        }
 
-    current = [x for x in range(0, len)]  # placeholder for our current value to change
+    # construct the leaves using the remaining values
+    # leaves = [make_tree(remaining[i], remaining) for i in range(0, len(remaining))]
+    leaves = []
+    for num in remaining:
+        leaves.append(make_tree(datum + [num], [item for item in remaining if item != num]))
 
-    while True:
-        for x in range(0, len - 1):
-            current[x], current[x + 1] = current[x + 1], current[x]  # make the swap
+    return {
+        'p': datum,
+        'leaves': leaves
+    }
 
-            if current == starting_point:  # exit condition
-                return
 
-            yield current
+def get_leaves(tree):
+
+    leaves = []
+    for leaf in tree['leaves']:
+        if len(leaf['leaves']) == 0:
+            leaves.append(leaf['p'])
+        else:
+            leaves.extend(get_leaves(leaf))
+
+    return leaves
 
 
 def find_square_by_rearranging(grid):
     new_list = [x for x in grid]
 
-    for permutation in get_permutation(len(grid)):
+    x = make_tree([], [i for i in range(0, len(grid))])
+
+    for permutation in get_leaves(x):
         new_list = apply_permutation(grid, new_list, permutation)
         if is_magic(new_list):
             return new_list
@@ -70,9 +87,25 @@ def find_square_by_rearranging(grid):
     return False
 
 
+def num_possible_rearrangements(grid):
+    num = 0
+    new_list = [x for x in grid]
+
+    x = make_tree([], [i for i in range(0, len(grid))])
+    y = get_leaves(x)
+
+    for permutation in y:
+        new_list = apply_permutation(grid, new_list, permutation)
+        if is_magic(new_list):
+            num += 1
+
+    return num
+
+
 if __name__ == '__main__':
     tests = [
         [[15, 14, 1, 4], [12, 6, 9, 7], [2, 11, 8, 13], [5, 3, 16, 10]],
+
         [[20, 19, 38, 30, 31, 36, 64, 22],
          [8, 16, 61, 53, 1, 55, 32, 34],
          [33, 60, 25, 9, 26, 50, 13, 44],
@@ -80,17 +113,27 @@ if __name__ == '__main__':
          [58, 35, 2, 48, 10, 40, 46, 21],
          [62, 11, 54, 47, 45, 7, 5, 29],
          [18, 57, 17, 27, 63, 14, 49, 15],
-         [24, 3, 12, 42, 43, 52, 28, 56]]
+         [24, 3, 12, 42, 43, 52, 28, 56]],
+
+        [
+            [111, 129, 27, 38, 119, 73, 30, 11, 123, 144, 6, 59],
+            [33, 22, 118, 102, 51, 121, 79, 132, 15, 50, 42, 105],
+            [14, 91, 41, 7, 85, 116, 60, 125, 128, 70, 71, 62],
+            [69, 92, 87, 142, 4, 28, 103, 43, 37, 112, 76, 77],
+            [136, 84, 115, 55, 137, 97, 17, 32, 13, 35, 16, 133],
+            [2, 46, 68, 78, 141, 94, 47, 80, 81, 82, 58, 93],
+            [108, 36, 20, 1, 65, 45, 143, 64, 113, 109, 56, 110],
+            [99, 18, 12, 49, 100, 114, 72, 66, 107, 5, 138, 90],
+            [95, 83, 57, 135, 67, 53, 31, 19, 39, 126, 140, 25],
+            [8, 86, 130, 88, 44, 21, 131, 63, 101, 29, 117, 52],
+            [89, 61, 75, 48, 54, 74, 23, 96, 104, 98, 124, 24],
+            [106, 122, 120, 127, 3, 34, 134, 139, 9, 10, 26, 40]
+        ]
     ]
 
-    # print("Running test: get_permutation")
-    # for res in get_permutation(8):
-    #     print(res)
+    print("Testing find_square_by_rearranging...")
+    for res in map(num_possible_rearrangements, tests):
+        print(res)
 
-    for i in range(2, 10):
-        print(str(i) + ": " + str(len([x for x in get_permutation(i)])))
 
-    # print("Running test: all_unique")
-    # for res in map(find_square_by_rearranging, tests):
-    #     print(res)
 
